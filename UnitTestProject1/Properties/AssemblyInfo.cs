4ec2aadf47b0e937743c0ff566a6bb9572c2426d
VIC VIPER <omantin60@gmail.com>
2020-01-21 23:34:48 +0900

    テストプロジェクトを追加 金銭データCSVをローカルDBへデータテーブルへ格納するコードを追記。

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e1ab237-42d2-4ed0-9d86-657b999a4ac5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cf = new CSVFormatter(@"D:\Documents\WorkFolder\'19_12.csv");
+            cf.Write();
+        }
+    }
+
+    internal class CSVFormatter
+    {
+        internal CSVFormatter(string path){ Path = path; }
+        private string Path;
+        private IReadOnlyList<string[]> readlines;
+        private IReadOnlyList<string> outlines;
+        internal void Write()
+        {
+            IEnumerable<string> lines = System.IO.File.ReadAllLines(Path);
+            readlines =lines.Select(x => x.Split(',')).ToList<string[]>();
+            SetDate();
+            System.IO.File.WriteAllLines(Path, outlines, System.Text.Encoding.GetEncoding("shift_jis"));
+        }
+
+        internal void SetDate()
+        {
+            string onedate;
+            for(int index=0; index<readlines.Count; index++ )
+            {
+                onedate = readlines[index][0];
+                if (onedate == "") { readlines[index][0] = readlines[index - 1][0]; }
+            }
+            outlines = readlines.Select(x => string.Join(",", x)).ToList();
+        }
+    }
+
+}
