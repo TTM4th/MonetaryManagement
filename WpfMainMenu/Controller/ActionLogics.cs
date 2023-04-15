0741e1ad02df5a1c6a2450328c44e566973de598
VIC VIPER <omantin60@gmail.com>
2023-04-16 02:13:50 +0900

    WPF版メインウィンドウの追加

---
+++ WpfMainMenu/Controller/ActionLogics.cs
@@ -0,0 +1,30 @@
+﻿namespace WpfMainMenu.Controller
+{
+    class ActionLogics
+    {
+        /// <summary>
+        /// 送信元のフォームオブジェクトインスタンス
+        /// </summary>
+        private MainWindow ParentForm { get; }
+
+        private DataController DataController { get; }
+
+        internal ActionLogics(MainWindow menuForm)
+        {
+            ParentForm = menuForm;
+            DataController = new DataController();
+            ParentForm.TableNameComboBox.ItemsSource = DataController.MonthlyTableNames;
+        }
+
+        /// <summary>
+        /// 現在残高をフォームに反映させる
+        /// </summary>
+        internal void ReflectNowBalance()
+        {
+            ParentForm.NowBalance.Content = DataController.GetCurrentBalance().ToString();
+            //ParentForm.NowBalance.Update();
+        }
+
+
+    }
+}
---
+++ WpfMainMenu/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using WpfMainMenu.Controller;
+
+namespace WpfMainMenu
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ActionLogics = new ActionLogics(this);
+        }
+        private ActionLogics ActionLogics { get; }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            InsertMode.IsChecked = true;
+            TableNameComboBox.SelectedIndex = 0;
+            ActionLogics.ReflectNowBalance();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FundRegister.FrontEnd.RegisterFormAccessor.RunRegisterForm((string)TableNameComboBox.SelectedItem, WholeEdit.IsChecked ?? false);
+            this.Show();
+            ActionLogics.ReflectNowBalance();
+        }
+    }
+}
